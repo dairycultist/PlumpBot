19ca76af64f13fcc78f9dfb020d834c2cfac382a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-02 18:26:40 -0400

    Prepare generalization for handling multiple art sites

--- index.js
+++ index.js
@@ -26,48 +26,69 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+// https://www.pixiv.net/en/artworks/133419030
+// https://x.com/53hank/status/1951368034310103293
+// https://bsky.app/profile/ciel-goat.bsky.social/post/3lvf4kodkus2r
+// https://twitter.com/VeryFilthyThing/status/1951406765305676282
+
 // message handling
 client.on(Events.MessageCreate, message => {
 
     if (message.author.bot) { return; }
 
-    if (!message.content.includes(" ") && message.content.startsWith("https://www.deviantart.com/")) {
+    console.log("'" + message.content + "'");
 
-        fetch(message.content.replace("deviantart", "fixdeviantart"))
-        .then(response => {
-            if (!response.ok)
-                throw new Error(`HTTP error! status: ${response.status}`);
-            return response.text(); // or .json()
-        })
-        .then(data => {
+    if (!message.content.includes(" ")) {
 
-            const image = /<meta property="og:image" content="([^"]*)">/.exec(data)[1];
-            const title = /<meta property="og:title" content="([^"]*)">/.exec(data)[1];
+        if (message.content.startsWith("https://www.deviantart.com/")) {
 
-            const exampleEmbed = {
-                color: 0x05CC46,
-                title: title,
-                url: message.content,
-                author: {
-                    name: "DeviantArt",
-                    icon_url: "https://images.icon-icons.com/2972/PNG/512/deviantart_logo_icon_186874.png"
-                },
-                image: { url: image },
-                footer: {
-                    text: "Sent by " + message.author.displayName,
-                    icon_url: message.author.avatarURL()
-                },
-            };
-
-            client.channels.cache.get(message.channelId).send({ embeds: [exampleEmbed] });
-            message.delete();
-            
-        })
-        .catch(error => {
-            console.error("There was a problem with the fetch operation: ", error);
-        });
+            inlineEmbedArt(
+                "DeviantArt",
+                "https://images.icon-icons.com/2972/PNG/512/deviantart_logo_icon_186874.png",
+                0x05CC46,
+                message.content,
+                message.content.replace("deviantart", "fixdeviantart")
+            );
+        }
     }
 });
+
+function inlineEmbedArt(siteName, siteImg, color, url, fixUrl) {
+
+    fetch(fixUrl)
+    .then(response => {
+        if (!response.ok)
+            throw new Error(`HTTP error! status: ${response.status}`);
+        return response.text();
+    })
+    .then(data => {
+
+        const image = /<meta property="og:image" content="([^"]*)">/.exec(data)[1];
+        const title = /<meta property="og:title" content="([^"]*)">/.exec(data)[1];
+
+        const exampleEmbed = {
+            color: color,
+            title: title,
+            url: url,
+            author: {
+                name: siteName,
+                icon_url: siteImg
+            },
+            image: { url: image },
+            footer: {
+                text: "Sent by " + message.author.displayName,
+                icon_url: message.author.avatarURL()
+            },
+        };
+
+        client.channels.cache.get(message.channelId).send({ embeds: [exampleEmbed] });
+        message.delete();
+        
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation: ", error);
+    });
+}
 
 // start bot
 client.login(token);
