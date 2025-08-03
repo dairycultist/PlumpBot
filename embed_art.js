c7e839c1812e4bfbf74dfa4d72ff127cc77ad47a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-03 01:45:22 -0400

    Genericize fetching so that we can more easily treat each platform differently, i.e. using its API

--- embed_art.js
+++ embed_art.js
@@ -5,15 +5,24 @@ function attemptEmbedArtFromMessage(client, message) {
 
         if (message.content.startsWith("https://www.deviantart.com/")) {
 
-            embedArt(
-                client,
-                message,
-                "DeviantArt",
-                "https://images.icon-icons.com/2972/PNG/512/deviantart_logo_icon_186874.png",
-                0x05CC46,
-                message.content,
-                message.content.replace("deviantart", "fixdeviantart")
-            );
+            fetchCallback(message.content.replace("deviantart", "fixdeviantart"), (html) => {
+
+                const image = parseMeta(html, "og:image");
+                const title = parseMeta(html, "og:title").replaceAll("&apos;", "'");
+                const description = parseMeta(html, "og:description").replaceAll("&apos;", "'");
+
+                embedArt(
+                    client,
+                    message,
+                    "DeviantArt",
+                    "https://images.icon-icons.com/2972/PNG/512/deviantart_logo_icon_186874.png",
+                    0x05CC46,
+                    title,
+                    description,
+                    message.content,
+                    image
+                );
+            });
 
         } else if (message.content.includes("/status/") && (message.content.startsWith("https://twitter.com/") || message.content.startsWith("https://x.com/"))) {
 
@@ -25,15 +34,30 @@ function attemptEmbedArtFromMessage(client, message) {
 
         } else if (message.content.includes("/post/") && message.content.startsWith("https://bsky.app/profile/")) {
 
-            embedArt(
-                client,
-                message,
-                "Bluesky",
-                "https://cdn.bsky.app/img/avatar/plain/did:plc:z72i7hdynmk6r22z27h6tvur/bafkreihagr2cmvl2jt4mgx3sppwe2it3fwolkrbtjrhcnwjk4jdijhsoze@jpeg",
-                0x4F9BD9,
-                message.content,
-                message.content
-            );
+            fetchCallback(message.content, (html) => {
+
+                const image = parseMeta(html, "og:image");
+                const title = parseMeta(html, "og:title").replaceAll("&apos;", "'");
+                const description = parseMeta(html, "og:description").replaceAll("&apos;", "'");
+
+                // https://docs.bsky.app/docs/api/app-bsky-feed-get-posts
+
+                // https://public.api.bsky.app/xrpc/app.bsky.feed.getPosts?uris=URI_FROM_LINK_BELOW
+
+                // <link rel="alternate" href="at://did:plc:ha27j5253cvi5s4mjdcxuzo3/app.bsky.feed.post/3lk54j7zz6s2u" />
+
+                embedArt(
+                    client,
+                    message,
+                    "Bluesky",
+                    "https://cdn.bsky.app/img/avatar/plain/did:plc:z72i7hdynmk6r22z27h6tvur/bafkreihagr2cmvl2jt4mgx3sppwe2it3fwolkrbtjrhcnwjk4jdijhsoze@jpeg",
+                    0x4F9BD9,
+                    title,
+                    description,
+                    message.content,
+                    image
+                );
+            });
 
         } else if (message.content.includes("/artworks/") && message.content.startsWith("https://www.pixiv.net/")) {
 
@@ -65,45 +89,42 @@ function parseMeta(data, property) {
     return "";
 }
 
-function embedArt(client, message, siteName, siteImg, color, url, fixUrl) {
+function fetchCallback(url, callback) {
 
-    fetch(fixUrl)
+    fetch(url)
     .then(response => {
         if (!response.ok)
             throw new Error(`HTTP error! status: ${response.status}`);
         return response.text();
     })
-    .then(data => {
-
-        console.log(data); // for testing
-
-        const image = parseMeta(data, "og:image");
-        const title = parseMeta(data, "og:title").replaceAll("&apos;", "'");
-        const description = parseMeta(data, "og:description").replaceAll("&apos;", "'");
-
-        const exampleEmbed = {
-            color: color,
-            title: title,
-            url: url,
-            author: {
-                name: siteName,
-                icon_url: siteImg
-            },
-            description: description,
-            image: { url: image },
-            footer: {
-                text: "Sent by " + message.author.displayName,
-                icon_url: message.author.avatarURL()
-            },
-        };
-
-        client.channels.cache.get(message.channelId).send({ embeds: [exampleEmbed] });
-        message.delete();
-        
+    .then(html => {
+        callback(html);
     })
     .catch(error => {
         console.error("There was a problem with the fetch operation: ", error);
     });
+}
+
+function embedArt(client, message, siteName, siteImg, color, title, description, url, image) {
+
+    const embed = {
+        color: color,
+        title: title,
+        url: url,
+        author: {
+            name: siteName,
+            icon_url: siteImg
+        },
+        description: description,
+        image: { url: image },
+        footer: {
+            text: "Sent by " + message.author.displayName,
+            icon_url: message.author.avatarURL()
+        },
+    };
+
+    client.channels.cache.get(message.channelId).send({ embeds: [ embed ] });
+    message.delete();
 }
 
 module.exports = {
