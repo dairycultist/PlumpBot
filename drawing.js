32bd67712453c06457efa10023d8e51d84e4273d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-29 22:15:41 -0500

    Integrate webui putter-upper as a bot command

--- drawing.js
+++ drawing.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const { firefox } = require("playwright"); // npm install playwright
 const { MessageFlags, SlashCommandBuilder, PermissionFlagsBits, REST, Routes, AttachmentBuilder } = require("discord.js");
 
 // mini goal is to have the bot take in a paperspace key as part of the config, and having a "start drawing backend" and "stop drawing backend" command, creating the notebook, starting it, and storing the gradio link
@@ -61,6 +63,103 @@ async function generateImage(prompt) {
 }
 
 const commands = [
+	{
+		data: new SlashCommandBuilder()
+            .setName("startwebui")
+            .setDescription("Puts up the gradio WebUI.")
+            .toJSON(),
+		execute: async (interaction, getArgValue) => {
+
+			// use the playwright automated browser client to put up the webui
+			// https://playwright.dev/
+			// await page.screenshot({ path: "screenshot.png" });
+
+			await interaction.channel.send(`Putting up WebUI (I will send AN IMAGE CONTAINING THE LINK in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
+
+			fs.readFile("./login.env", "utf8", async (err, data) => {
+
+				const sleep = async (sec) => {
+					return new Promise(resolve => setTimeout(resolve, sec * 1000));
+				};
+
+				if (err) {
+					console.error("Error reading file: ", err);
+					return;
+				}
+
+				data = data.split("\n");
+
+				const email = data[0];
+				const password = data[1];
+
+				// open playwright window
+				const browser = await firefox.launch({ headless: false }); // set to false to see what it's doing
+				const page = await browser.newPage();
+
+				// log in
+				await page.goto("https://console.paperspace.com/login");
+				
+				await page.locator(`[name="email"]`).fill(email);
+				await page.locator(`[name="password"]`).fill(password);
+				
+				await page.getByRole("button", { name: "Log in" }).click();
+
+				await sleep(5);
+
+				// go to notebook page
+				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn");
+				
+				await sleep(5);
+
+				// stop notebook if it's running to ensure predictable state
+				{
+					const button = page.getByRole("button", { name: "Stop machine" });
+
+					if (await button.count() != 0)
+						await button.click();
+				}
+
+				// start notebook
+				await page.getByRole("button", { name: "Start machine" }).click();
+				await sleep(20);
+
+				// run launcher
+				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flauncher.ipynb");
+
+				await page.getByRole("button", { name: "Run all" }).click();
+
+				// run looper
+				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flooper.ipynb");
+
+				await page.getByRole("button", { name: "Run all" }).click();
+
+				// open terminal
+				await page.locator(`[aria-controls="radix-4-content-terminals"]`).click();
+
+				await page.mouse.click(80, 100); // clicking doesn't always have a chance of working on the button so I just press it a bunch to ensure a terminal pops up
+				await page.mouse.click(80, 100); // for some reason the selector didn't work, maybe because of the same click-dropping thing
+				await page.mouse.click(80, 100);
+				await page.mouse.click(80, 100);
+
+				await sleep(60);
+
+				await page.locator(`[aria-label="Terminal input"]`).fill("cd /notebooks/stable-diffusion-webui\nsource venv/bin/activate\n./webui.sh\n");
+
+				await sleep(70);
+
+				await page.locator(`[class="xterm-text-layer"]`).screenshot({ animations: "disabled", path: "link.png" });
+
+				await interaction.channel.send(`Putting up WebUI (I will send the link in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
+
+				await sleep(5);
+
+				await interaction.channel.send({ files: [{ attachment: "link.png" }] });
+
+				await browser.close();
+			});
+
+		}
+	},
     {
         data: new SlashCommandBuilder()
             .setName("setgradioid")
