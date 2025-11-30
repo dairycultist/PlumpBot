4a327b1bb7507d68bbab96bd6cefddbd244225fc
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-29 20:43:30 -0500

    Login

--- test_playwright.js
+++ test_playwright.js
@@ -1,12 +1,32 @@
 const { firefox } = require("playwright"); // npm install playwright
+const fs = require("fs");
 
-(async () => {
-	const browser = await firefox.launch();
+fs.readFile("./login.env", "utf8", async (err, data) => {
+
+	if (err) {
+		console.error("Error reading file: ", err);
+		return;
+	}
+
+	data = data.split("\n");
+
+	const email = data[0];
+	const password = data[1];
+
+	// open playwright window
+	const browser = await firefox.launch({ headless: false });
 	const page = await browser.newPage();
 	await page.goto("https://console.paperspace.com/login");
-  
+	
+	await page.locator(`[name="email"]`).fill(email);
+	await page.locator(`[name="password"]`).fill(password);
+	
+	await page.getByRole("button", { name: "Log in" }).click();
+
+	await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+
 	await page.screenshot({ path: "screenshot.png" });
-	await page.locator(`[name="email"]`).screenshot({ animations: 'disabled', path: 'screenshot.png' });
 
 	await browser.close();
-})();
+
+});
