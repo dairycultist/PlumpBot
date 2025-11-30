18f5e2b1babef3bab41801df39e48e1efd9b4b79
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-11-29 22:48:37 -0500

    Refactor

--- drawing.js
+++ drawing.js
@@ -74,89 +74,96 @@ const commands = [
 			// https://playwright.dev/
 			// await page.screenshot({ path: "screenshot.png" });
 
-			await interaction.channel.send(`Putting up WebUI (I will send AN IMAGE CONTAINING THE LINK in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
+			await interaction.reply(`Putting up WebUI (I will send AN IMAGE CONTAINING THE LINK in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
 
-			fs.readFile("./login.env", "utf8", async (err, data) => {
+			try {
 
-				const sleep = async (sec) => {
-					return new Promise(resolve => setTimeout(resolve, sec * 1000));
-				};
+				fs.readFile("./login.env", "utf8", async (err, data) => {
 
-				if (err) {
-					console.error("Error reading file: ", err);
-					return;
-				}
+					const sleep = async (sec) => {
+						return new Promise(resolve => setTimeout(resolve, sec * 1000));
+					};
 
-				data = data.split("\n");
+					if (err) {
+						console.error("Error reading file: ", err);
+						return;
+					}
 
-				const email = data[0];
-				const password = data[1];
+					data = data.split("\n");
 
-				// open playwright window
-				const browser = await firefox.launch({ headless: false }); // set to false to see what it's doing
-				const page = await browser.newPage();
+					const email = data[0];
+					const password = data[1];
 
-				// log in
-				await page.goto("https://console.paperspace.com/login");
-				
-				await page.locator(`[name="email"]`).fill(email);
-				await page.locator(`[name="password"]`).fill(password);
-				
-				await page.getByRole("button", { name: "Log in" }).click();
+					// open playwright window
+					const browser = await firefox.launch({ headless: false }); // set to false to see what it's doing
+					const page = await browser.newPage();
 
-				await sleep(5);
+					// log in
+					await page.goto("https://console.paperspace.com/login");
+					
+					await page.locator(`[name="email"]`).fill(email);
+					await page.locator(`[name="password"]`).fill(password);
+					
+					await page.getByRole("button", { name: "Log in" }).click();
 
-				// go to notebook page
-				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn");
-				
-				await sleep(5);
+					await sleep(5);
 
-				// stop notebook if it's running to ensure predictable state
-				{
-					const button = page.getByRole("button", { name: "Stop machine" });
+					// go to notebook page
+					await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn");
+					
+					await sleep(5);
 
-					if (await button.count() != 0)
-						await button.click();
-				}
+					// stop notebook if it's running to ensure predictable state
+					{
+						const button = page.getByRole("button", { name: "Stop machine" });
 
-				// start notebook
-				await page.getByRole("button", { name: "Start machine" }).click();
-				await sleep(20);
+						if (await button.count() != 0)
+							await button.click();
+					}
 
-				// run launcher
-				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flauncher.ipynb");
+					// start notebook
+					await page.getByRole("button", { name: "Start machine" }).click();
+					await sleep(20);
 
-				await page.getByRole("button", { name: "Run all" }).click();
+					// run launcher
+					await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flauncher.ipynb");
 
-				// run looper
-				await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flooper.ipynb");
+					await page.getByRole("button", { name: "Run all" }).click();
 
-				await page.getByRole("button", { name: "Run all" }).click();
+					// run looper
+					await page.goto("https://console.paperspace.com/tbp1l86qmb/notebook/rzaf4sanl19bidn?file=%2Flooper.ipynb");
 
-				// open terminal
-				await page.locator(`[aria-controls="radix-4-content-terminals"]`).click();
+					await page.getByRole("button", { name: "Run all" }).click();
 
-				await page.mouse.click(80, 100); // clicking doesn't always have a chance of working on the button so I just press it a bunch to ensure a terminal pops up
-				await page.mouse.click(80, 100); // for some reason the selector didn't work, maybe because of the same click-dropping thing
-				await page.mouse.click(80, 100);
-				await page.mouse.click(80, 100);
+					// open terminal
+					await page.locator(`[aria-controls="radix-4-content-terminals"]`).click();
 
-				await sleep(60);
+					await page.mouse.click(80, 100); // clicking doesn't always have a chance of working on the button so I just press it a bunch to ensure a terminal pops up
+					await page.mouse.click(80, 100); // for some reason the selector didn't work, maybe because of the same click-dropping thing
+					await page.mouse.click(80, 100);
+					await page.mouse.click(80, 100);
 
-				await page.locator(`[aria-label="Terminal input"]`).fill("cd /notebooks/stable-diffusion-webui\nsource venv/bin/activate\n./webui.sh\n");
+					await sleep(60);
 
-				await sleep(70);
+					await page.locator(`[aria-label="Terminal input"]`).fill("cd /notebooks/stable-diffusion-webui\nsource venv/bin/activate\n./webui.sh\n");
 
-				await page.locator(`[class="xterm-text-layer"]`).screenshot({ animations: "disabled", path: "link.png" });
+					await sleep(70);
 
-				await interaction.channel.send(`Putting up WebUI (I will send the link in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
+					await page.locator(`[class="xterm-text-layer"]`).screenshot({ animations: "disabled", path: "link.png" });
 
-				await sleep(5);
+					await interaction.channel.send(`Putting up WebUI (I will send the link in this channel once it's up). This may take a while! This command is not very fieldtested --- if after >5 minutes it doesn't send anything, try again, but please be patient!`);
 
-				await interaction.channel.send({ files: [{ attachment: "link.png" }] });
+					await sleep(5);
 
-				await browser.close();
-			});
+					await interaction.channel.send({ files: [{ attachment: "link.png" }] });
+
+					await browser.close();
+				});
+
+			} catch (e) {
+
+				await interaction.channel.send(`Error: \`\`\`${ e }\`\`\``);
+			}
 
 		}
 	},
